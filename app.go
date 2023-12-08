11d1b36a40e11dbc00aecc66700085af28b78038
malaohu <malaohu@live.cn>
2023-12-08 10:59:22 +0800

    update

--- app.go
+++ app.go
@@ -100,13 +100,25 @@ func write_file(content string){
 
 
 func get_ip(url string) string {
-	resp, err := grequests.Get("https://ipaddress.com/website/"+url, nil)
+	
+	ro := &grequests.RequestOptions{
+		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36",
+
+		//Headers: map[string]string{
+		//	"Accept":          "*/*",
+		//	"Accept-Encoding": "gzip, deflate, br",
+		//	"Connection":      "keep-alive",
+		//	"Cookie":          "ezoadgid_280870=-1; ezoref_280870=; ezosuibasgeneris-1=c20e8ba9-e495-4415-5c34-e3daa3d80321; ezoab_280870=mod1; lp_280870=https://sites.ipaddress.com/alive.github.com/; ezovuuid_280870=c991149d-3189-4f59-77e0-a1762c2c8385; ezds=ffid%3D1%2Cw%3D1920%2Ch%3D1080; ezohw=w%3D1920%2Ch%3D923; _ga=GA1.1.1440838470.1702002556; __qca=P0-410538264-1702002563689; active_template::280870=pub_site.1702002635; ezopvc_280870=4; ezovuuidtime_280870=1702002635; _ga_3GT2CLN45N=GS1.1.1702002556.1.1.1702002635.0.0.0; ezux_lpl_280870=1702002636435|32f307ab-be94-465d-499d-a271e85bf881|false",
+		//},
+	}
+
+	resp, err := grequests.Get("https://ipaddress.com/website/"+url, ro)
 
 	if err != nil {
 		log.Fatalln("Unable to make request: ", err)
 	}
 
-	reg := regexp.MustCompile(`<li>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</li>`)
+	reg := regexp.MustCompile(`>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</a>`)
 
 	result := reg.FindAllStringSubmatch(resp.String(), -1)
 
